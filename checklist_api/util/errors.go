2b0feea1174eb5d1915dcbb77c01050b97caf8f3
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-09 20:52:06 +0100

    Update errors.go

--- checklist_api/util/errors.go
+++ checklist_api/util/errors.go
@@ -1,67 +1,56 @@
 package util
 
 import (
+	"checklist/checklist_api/model"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type ErrResponse struct {
-	StatusCode uint16 `json:"status_code,omitempty"`
-	Message    string `json:"message,omitempty"`
-	ErrorText  string `json:"error,omitempty"`
-}
-
 func BadRequest(c *gin.Context, err error) {
-	e := ErrResponse{
-		StatusCode: http.StatusBadRequest,
-		ErrorText:  "Bad Request",
-		Message:    err.Error(),
+	e := model.Response{
+		Status:  http.StatusBadRequest,
+		Message: err.Error(),
 	}
 	c.AbortWithStatusJSON(http.StatusBadRequest, e)
 }
 
 func UnprocessableEntity(c *gin.Context, err error) {
-	e := ErrResponse{
-		StatusCode: http.StatusUnprocessableEntity,
-		ErrorText:  "Unprocessable Entity",
-		Message:    err.Error(),
+	e := model.Response{
+		Status:  http.StatusUnprocessableEntity,
+		Message: err.Error(),
 	}
 	c.AbortWithStatusJSON(http.StatusUnprocessableEntity, e)
 }
 
 func InternalServerError(c *gin.Context, err error) {
-	e := ErrResponse{
-		StatusCode: http.StatusInternalServerError,
-		ErrorText:  "Internal Server Error",
-		Message:    err.Error(),
+	e := model.Response{
+		Status:  http.StatusInternalServerError,
+		Message: err.Error(),
 	}
 	c.AbortWithStatusJSON(http.StatusInternalServerError, e)
 }
 
 func Unauthorized(c *gin.Context, err error) {
-	e := ErrResponse{
-		StatusCode: http.StatusUnauthorized,
-		ErrorText:  "Unauthorized",
-		Message:    err.Error(),
+	e := model.Response{
+		Status:  http.StatusUnauthorized,
+		Message: err.Error(),
 	}
 	c.AbortWithStatusJSON(http.StatusUnauthorized, e)
 }
 
 func Forbidden(c *gin.Context, err error) {
-	e := ErrResponse{
-		StatusCode: http.StatusForbidden,
-		ErrorText:  "Forbidden",
-		Message:    err.Error(),
+	e := model.Response{
+		Status:  http.StatusForbidden,
+		Message: err.Error(),
 	}
 	c.AbortWithStatusJSON(http.StatusForbidden, e)
 }
 
 func NotFound(c *gin.Context, err error) {
-	e := ErrResponse{
-		StatusCode: http.StatusNotFound,
-		ErrorText:  "Not Found",
-		Message:    err.Error(),
+	e := model.Response{
+		Status:  http.StatusNotFound,
+		Message: err.Error(),
 	}
 	c.AbortWithStatusJSON(http.StatusNotFound, e)
 }
