2d93862c2373f3cfd052041deb19ca92c2ec7b74
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-11 22:42:06 +0100

    Update platform_icons.dart

--- checklist_app/lib/core/platform_specific/platform_icons.dart
+++ checklist_app/lib/core/platform_specific/platform_icons.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 class AppIcons {
   static get check => Iconsax.tick_square;
   static get add => Iconsax.add;
-  static get profile => Iconsax.profile_circle;
+  static get setting => Iconsax.setting;
   static get logout => Iconsax.logout;
+  static get clear => Icons.clear_rounded;
 }
