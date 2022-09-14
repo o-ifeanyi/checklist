482425e957e6ae159d089057eaae515e5e73439c
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-14 09:48:36 +0100

    Create auth_service_mock.go

---
+++ checklist_api/mock/auth_service_mock.go
@@ -0,0 +1,32 @@
+package mock
+
+import (
+	"checklist/checklist_api/model"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type AuthService struct {
+	mock.Mock
+}
+
+func (w AuthService) FindById(userID string) (model.User, error) {
+	args := w.Called(userID)
+	return model.User{}, args.Error(1)
+}
+func (w AuthService) FindByEmail(email string) (model.User, error) {
+	args := w.Called(email)
+	return model.User{}, args.Error(1)
+}
+func (w AuthService) Create(email, password string) (string, error) {
+	args := w.Called(email, password)
+	return args.String(0), args.Error(1)
+}
+func (w AuthService) Login(email, password string) (string, error) {
+	args := w.Called(email, password)
+	return args.String(0), args.Error(1)
+}
+func (w AuthService) Logout(userId string) error {
+	args := w.Called(userId)
+	return args.Error(0)
+}
