61c018c11dbf0090f2f2f2d061d616d4849c7159
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-15 23:41:09 +0100

    Update platform_icons.dart

--- checklist_app/lib/core/platform_specific/platform_icons.dart
+++ checklist_app/lib/core/platform_specific/platform_icons.dart
@@ -5,6 +5,8 @@ class AppIcons {
   static get check => Iconsax.tick_square;
   static get add => Iconsax.add;
   static get setting => Iconsax.setting;
+  static get delete => Iconsax.trash;
   static get logout => Iconsax.logout;
   static get clear => Icons.clear_rounded;
+  static get search => Iconsax.search_normal_14;
 }
