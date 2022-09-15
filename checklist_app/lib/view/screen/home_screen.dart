b8fc04714bd5287aa55555bb7d8741a003a4b8d1
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-15 23:44:29 +0100

    sync on resume / delete multiple items

--- checklist_app/lib/view/screen/home_screen.dart
+++ checklist_app/lib/view/screen/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:checklist_app/core/util/config.dart';
 import 'package:checklist_app/provider/checklist_provider.dart';
 import 'package:checklist_app/view/screen/checklist_screen.dart';
 import 'package:checklist_app/view/screen/profile_screen.dart';
+import 'package:checklist_app/view/widget/checklist_item.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:go_router/go_router.dart';
@@ -18,120 +20,124 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  late ChecklistProvider provider;
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
+  late ChecklistProvider _provider;
+  late ValueListenable<Box<dynamic>> _checklistBox;
+  bool _markAll = false;
 
   @override
   void initState() {
     super.initState();
-    provider = context.read<ChecklistProvider>();
+    _provider = context.read<ChecklistProvider>();
+    WidgetsBinding.instance.addObserver(this);
+    _checklistBox = Hive.box(kHiveBoxes.checklist.name).listenable();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      provider.getAll();
+      _provider.getAll();
+      _checklistBox.addListener(() {
+        _provider.getAll();
+      });
     });
   }
 
   @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _checklistBox.removeListener(() {});
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      _provider.sync();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final provider = context.watch<ChecklistProvider>();
-    final checklists = provider.allChecklist;
-    final theme = Theme.of(context);
+    final _provider = context.watch<ChecklistProvider>();
+    final checklists = _provider.allChecklist;
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
+            Container(
+              height: Config.yMargin(context, 11),
               padding: Config.contentPadding(context),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      style: Config.b1(context),
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                        errorStyle: Config.b2(context),
-                        contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 12, vertical: 8),
-                      ),
-                      onChanged: (val) {},
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  GestureDetector(
-                    onTap: () => context.push(ProfileScreen.route),
-                    child: Icon(
-                      AppIcons.setting,
-                      size: 30,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(height: Config.yMargin(context, 2)),
-            Expanded(
-              child: ValueListenableBuilder(
-                valueListenable:
-                    Hive.box(kHiveBoxes.checklist.name).listenable(),
-                builder: (context, _, child) {
-                  return MasonryGridView.builder(
-                    padding: Config.contentPadding(context),
-                    itemCount: checklists.length,
-                    gridDelegate:
-                        SliverSimpleGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: Config.xMargin(context, 50),
-                    ),
-                    crossAxisSpacing: Config.xMargin(context, 3),
-                    mainAxisSpacing: Config.yMargin(context, 2),
-                    itemBuilder: (context, index) {
-                      final checklist = checklists[index];
-                      return GestureDetector(
-                        onTap: () {
-                          context.push(ChecklistScreen.route, extra: checklist);
-                        },
-                        child: Container(
-                          padding: Config.contentPadding(context, h: 3, v: 1),
-                          height: checklist.extent(context),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                color: theme.colorScheme.secondary
-                                    .withOpacity(0.6),
-                              ),
-                              borderRadius: BorderRadius.circular(12)),
-                          child: Column(
-                            children: [
-                              Text(
-                                checklist.title,
-                                overflow: TextOverflow.ellipsis,
-                                style: Config.h2(context),
-                              ),
-                              SizedBox(height: Config.yMargin(context, 1)),
-                              ...checklist.undone
-                                  .take(4)
-                                  .map(
-                                    (e) => CheckboxListTile(
-                                      dense: true,
-                                      contentPadding: const EdgeInsets.all(0),
-                                      controlAffinity:
-                                          ListTileControlAffinity.leading,
-                                      title: Text(
-                                        e.text,
-                                        style: Config.b1(context),
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      checkboxShape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      value: false,
-                                      onChanged: null,
-                                    ),
-                                  )
-                                  .toList(),
-                            ],
+              child: _provider.marked.isNotEmpty
+                  ? Row(
+                      children: [
+                        Expanded(
+                          child: CheckboxListTile(
+                            dense: true,
+                            contentPadding: const EdgeInsets.all(0),
+                            controlAffinity: ListTileControlAffinity.leading,
+                            title: Text(
+                              'Select all  (${_provider.marked.length})',
+                              style: Config.b1b(context),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            value: _markAll,
+                            onChanged: (val) {
+                              if (val == null) return;
+                              _markAll = val;
+                              _provider.markAll(val);
+                            },
                           ),
                         ),
-                      );
-                    },
-                  );
+                        const Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            _provider.delete(_provider.marked).then((sucess) {
+                              _provider.markAll(false);
+                            });
+                          },
+                          child: Icon(
+                            AppIcons.delete,
+                          ),
+                        )
+                      ],
+                    )
+                  : Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            style: Config.b1(context),
+                            decoration: InputDecoration(
+                              labelText: 'Search',
+                              prefixIcon: Icon(AppIcons.search),
+                              errorStyle: Config.b2(context),
+                              contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 8),
+                            ),
+                            onChanged: (val) {},
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        GestureDetector(
+                          onTap: () => context.push(ProfileScreen.route),
+                          child: Icon(
+                            AppIcons.setting,
+                            size: 30,
+                          ),
+                        )
+                      ],
+                    ),
+            ),
+            Expanded(
+              child: MasonryGridView.builder(
+                padding: Config.contentPadding(context),
+                itemCount: checklists.length,
+                gridDelegate: SliverSimpleGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: Config.xMargin(context, 50),
+                ),
+                crossAxisSpacing: Config.xMargin(context, 3),
+                mainAxisSpacing: Config.yMargin(context, 2),
+                itemBuilder: (context, index) {
+                  final checklist = checklists[index];
+                  return ChecklistItem(checklist: checklist);
                 },
               ),
             ),
