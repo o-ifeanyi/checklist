a649a01ea6d37726f2ebede3b2b19a2c8ba930a1
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-15 23:42:14 +0100

    Avoid duplicate connectivity results / sync with network

--- checklist_app/lib/core/services/connection_service.dart
+++ checklist_app/lib/core/services/connection_service.dart
@@ -1,22 +1,28 @@
 import 'dart:async';
 
+import 'package:checklist_app/injection_container.dart';
 import 'package:checklist_app/interface/checklist_repository.dart';
+import 'package:checklist_app/provider/checklist_provider.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectionService {
   final Connectivity connectivity;
   final ChecklistRepository checklistRepository;
+  ConnectivityResult _connectivityResult = ConnectivityResult.none;
 
   ConnectionService({
     required this.connectivity,
     required this.checklistRepository,
   }) {
     connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
-      _connectivityStreamController.add(result);
+      if (result != _connectivityResult) {
+        _connectivityStreamController.add(result);
+        _connectivityResult = result;
+      }
       switch (result) {
         case ConnectivityResult.mobile:
         case ConnectivityResult.wifi:
-          // sync data
+          sl<ChecklistProvider>().sync();
           break;
         default:
       }
