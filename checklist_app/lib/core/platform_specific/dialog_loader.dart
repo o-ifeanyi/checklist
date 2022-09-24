90bdb4cd8ea48c2d959e9e676d790c76d94ca357
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-24 21:12:01 +0100

    Update dialog_loader.dart

--- checklist_app/lib/core/platform_specific/dialog_loader.dart
+++ checklist_app/lib/core/platform_specific/dialog_loader.dart
@@ -4,26 +4,36 @@ import 'package:flutter/material.dart';
 import 'platform_alert_dialog.dart';
 import 'platform_progress_indicator.dart';
 
-class DialogLoader<T> extends StatelessWidget {
-  final Future<T> function;
+class DialogLoader<T> extends StatefulWidget {
+  final Future<bool?> Function() function;
   const DialogLoader({Key? key, required this.function}) : super(key: key);
 
   @override
+  State<DialogLoader<T>> createState() => _DialogLoaderState<T>();
+}
+
+class _DialogLoaderState<T> extends State<DialogLoader<T>> {
+  Future<void> call() async {
+    final done = await widget.function.call();
+    Navigator.pop(context, done);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      call();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<dynamic>(
-      future: function,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          Navigator.pop(context, snapshot.data);
-        }
-        return PlatformAlertDialog(
-          content: Padding(
-            padding: Config.contentPadding(context, v: 3),
-            child: const PlatformProgressIndicator(),
-          ),
-          actions: [],
-        );
-      },
+    return PlatformAlertDialog(
+      content: Padding(
+        padding: Config.contentPadding(context, v: 3),
+        child: const PlatformProgressIndicator(),
+      ),
+      actions: [],
     );
   }
 }
