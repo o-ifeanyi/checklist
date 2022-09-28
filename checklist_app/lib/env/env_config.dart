b03e91506def85b5583f1457b0423ea61015e7c0
o-ifeanyi <onuifeanyi95@gmail.com>
2022-09-28 12:04:17 +0100

    Fix android localhost issue

--- checklist_app/lib/env/env_config.dart
+++ checklist_app/lib/env/env_config.dart
@@ -1,3 +1,4 @@
+import 'package:checklist_app/core/util/config.dart';
 import 'package:checklist_app/env/secrets.dart';
 
 abstract class BaseConfig {
@@ -6,7 +7,8 @@ abstract class BaseConfig {
 
 class StagingEnv implements BaseConfig {
   @override
-  String get baseUrl => 'http://localhost:8080/';
+  String get baseUrl =>
+      Config.isAndroid ? 'http://10.0.2.2:8080/' : 'http://localhost:8080/';
 }
 
 class ProductionEnv implements BaseConfig {
